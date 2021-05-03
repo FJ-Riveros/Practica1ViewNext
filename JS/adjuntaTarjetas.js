e578b87b3bf2263be5a3ecb031aef1bcfd2759d3
FJ-Riveros <fran.etnies@gmail.com>
2021-05-03 11:32:41 +0200

    Changes made to the logic of the Cards

--- JS/adjuntaTarjetas.js
+++ JS/adjuntaTarjetas.js
@@ -5,28 +5,30 @@ export function generadorCard() {
   let values = getValues();
   creaNuevaEntrada(values[0], values[1], values[2], values[3]);
   let entradas = obtenerEntradas();
-  adjuntarTarjeta(entradas);
+  destruyeDisplayCards();
+  muestraCardsActuales(entradas);
+  console.log(entradas);
 
   //Añadir Event Listenner a la tarjeta
-  listennerCard(`#Card-${entradas.length}`);
+  /*listennerCard(`#Card-${entradas.length}`);
+  $(`#Card-${entradas.length} div.card-header img.modify`).click(() =>
+    console.log("modify")
+  );
+  $(`#Card-${entradas.length} div.card-header img.delete`).click(() =>
+    console.log("delete")
+  );
   function listennerCard(idCard) {
     $(idCard).hover(
       function () {
         // over
         $(`${idCard} div.card-header img`).css("display", "unset");
-        $(`${idCard} div.card-header img.modify`).click(() =>
-          console.log("modify")
-        );
-        $(`${idCard} div.card-header img.delete`).click(() =>
-          console.log("delete")
-        );
       },
       function () {
         // out
         $(`${idCard} div.card-header img`).css("display", "none");
       }
     );
-  }
+  }*/
   //Vaciamos todos los campos
   vaciarCampos(".form-control");
   //Reseteamos los colores y mensajes del form
@@ -45,24 +47,24 @@ function getValues() {
 }
 
 //Crea el cuerpo de la entrada nueva
-export const adjuntarTarjeta = (registro) => {
+export const adjuntarTarjeta = (registro, numRegistro) => {
+  var imagenDelete = "media/basura.png";
+  var imagenModificar = "media/pencil.png";
   $(".tarjetas").append(
     `
     <div class="col-sm-4 mb-4">
-      <div class="card shadow" id=Card-${registro.length}>
-        <div class="card-header p-2 font-weight-bold light-gray">${
-          registro[registro.length - 1].nombre
-        }<img src="media/basura.png" width="32px" height="32px" class="delete">
-        <img src="media/pencil.png" widht="30px" height="30px" class="modify"></div>
+      <div class="card shadow" id=Card-${numRegistro}>
+        <div class="card-header p-2 font-weight-bold light-gray">${registro.nombre}<img src="${imagenDelete}" width="32px" height="32px" class="delete">
+        <img src="${imagenModificar}" widht="30px" height="30px" class="modify"></div>
         <div class=" text-secondary p-2 min-height texto-principal">
-          <p class="card-text">${registro[registro.length - 1].descripcion}</p>
+          <p class="card-text">${registro.descripcion}</p>
         </div>
         <div class="row justify-content-between p-2 text-secondary">
           <div class="col-4 font-weight-medium">
-              ${registro[registro.length - 1].precio}€
+              ${registro.precio}€
             </div>
             <div class=" col-5 flex-end font-weight-medium ">
-              Cantidad: ${registro[registro.length - 1].stock}
+              Cantidad: ${registro.stock}
             </div>
           </div>
       </div>
@@ -71,6 +73,15 @@ export const adjuntarTarjeta = (registro) => {
   );
 };
 
+function muestraCardsActuales(entradas) {
+  for (let i = 0; i < entradas.length; i++) {
+    adjuntarTarjeta(entradas[i], i + 1);
+  }
+}
+
+function destruyeDisplayCards() {
+  $(".tarjetas").html("");
+}
 /*$(".tarjetas").append(
     `
     <div class="col-sm-4 mb-4">
